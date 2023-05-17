1127428914a77c9966493d375a106f1ae81e0898
Maciek Danik <maciekdanik@gmail.com>
2023-05-17 14:21:24 +0200

    Single Left Rotation

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -127,3 +127,46 @@ void AVLTree<T>::singleRightRotation(AVLNode<T>* node)
 		tmpSon->setBF(1);
 	}
 }
+
+template<class T>
+void AVLTree<T>::singleLeftRotation(AVLNode<T>* node)
+{
+	AVLNode<T>* tmpSon = node->getLeftSon();
+	AVLNode<T>* tmpParent = node->getParent();
+
+	node->setLeftSon(tmpSon->getRightSon());
+
+	if (node->getLeftSon() != nullptr)
+	{
+		node->getLeftSon()->setParent(node);
+	}
+
+	tmpSon->setRightSon(node);
+	tmpSon->setParent(tmpParent);
+	node->setParent(tmpSon);
+
+	if (tmpParent == nullptr)
+	{
+		root = tmpSon;
+	}
+
+	if (tmpParent->getLeftSon() == node)
+	{
+		tmpParent->setLeftSon(tmpSon);
+	}
+	else
+	{
+		tmpParent->setRightSon(tmpSon);
+	}
+
+	if (tmpSon->getBF() == 1)
+	{
+		node->setBF(0);
+		tmpSon->setBF(0);
+	}
+	else
+	{
+		node->setBF(1);
+		tmpSon->setBF(-1);
+	}
+}
